98ae896701b7bd7c858c4e2b45578ef365f7fd10
scelester <nabinpauudel664@gmail.com>
2022-11-09 12:30:04 +0545

    extras 01

---
+++ pins_on_fire.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as gpio
+from time import sleep
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(14, gpio.OUT)
+
+food_dispenser_servo = gpio.PWM(14,50)
+
+
+while True:
+    food_dispenser_servo.start(0)
+    
+    # wait 2 seconds 
+    sleep(1)
+
+    duty = 2
+    while duty <= 17:
+        food_dispenser_servo.ChangeDutyCycle(duty)
+        sleep(1)
+        duty += 1
+        
+        
