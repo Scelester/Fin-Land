59189a6c8e69348bf552730f022933e131e3d04b
scelester <nabinpauudel664@gmail.com>
2022-11-09 13:32:12 +0545

    0011

--- pin_manager/food_servo.py
+++ pin_manager/food_servo.py
@@ -1,11 +1,13 @@
 
 def start_servo(servo,duty):
     servo.start(0)
-            
+    print("11111")
     while duty < 9:
+        print(222222)
         servo.ChangeDutyCycle(duty)
         duty += 3
         sleep(1)
+        print(33333)
 
     duty = 1
     sleep(3)
