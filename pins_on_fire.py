4da1df93fe7e098c1c4c10fdf2ccd912796a6f86
scelester <nabinpauudel664@gmail.com>
2022-11-09 12:45:51 +0545

    0011

--- pins_on_fire.py
+++ pins_on_fire.py
@@ -6,19 +6,20 @@ gpio.setup(14, gpio.OUT)
 
 food_dispenser_servo = gpio.PWM(14,50)
 
+duty = 5
+x = 0
+
 try:
     while True:
         food_dispenser_servo.start(0)
-        
-        # wait 2 seconds 
-        sleep(0.3)
-
-        duty = 1
-        while duty <= 20:
+        sleep(0.5)
+        if x < 20:
             food_dispenser_servo.ChangeDutyCycle(duty)
-            print("mooved")
-            sleep(0.5)
-            duty += 5
+        else:
+            break
+            print("loop broke")
+
+            
         
 except KeyboardInterrupt:
     print("programme stopped")
