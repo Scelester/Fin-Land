0ad7c64bc2f31d04d3d0339db94e25c399c009f4
scelester <nabinpauudel664@gmail.com>
2022-11-11 19:19:50 +0545

    testing phase 6

--- pin_manager/food_servo.py
+++ pin_manager/food_servo.py
@@ -5,15 +5,17 @@ def start_servo(servo,duty,STATE):
     print("================ servo started =========================")
 
     servo.ChangeDutyCycle(duty)
-    print("Changed duty cycle to initial value :1")
+    print(">> Changed duty cycle to initial value :1")
     sleep(1)
 
     duty = 20
-    
     servo.ChangeDutyCycle(duty)
-    
+    print(">> Changed duty cycle to initial value :20")
+
     sleep(5)
     duty = 1
     servo.ChangeDutyCycle(duty)
+    print(">> Resetting duty cycle to initial value :1")
+
     
     STATE = False
