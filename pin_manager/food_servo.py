88420e858878d22da33b006ba415e046228cf0d6
scelester <nabinpauudel664@gmail.com>
2022-11-11 18:57:07 +0545

    testing phase 6

--- pin_manager/food_servo.py
+++ pin_manager/food_servo.py
@@ -2,14 +2,8 @@ from time import sleep
 
 def start_servo(servo,duty):
     servo.start(0)
-    print("11111")
-    while duty < 12:
-        print(222222)
-        servo.ChangeDutyCycle(duty)
-        duty += 3
-        sleep(0.3)
-
+    print(222222)
+    servo.ChangeDutyCycle(20)
+    sleep(5)
     duty = 1
-    sleep(3)
     servo.ChangeDutyCycle(duty)
-    print("food 1 cycle")
