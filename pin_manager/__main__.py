89646f68272b140f4f3f7be2e500739279c18103
scelester <nabinpauudel664@gmail.com>
2022-11-11 19:13:52 +0545

    testing phase 6

--- pin_manager/__main__.py
+++ pin_manager/__main__.py
@@ -4,17 +4,20 @@ from time import sleep
 # importing files
 from food_servo import start_servo
 
-
+# setting up goio keys
 gpio.setmode(gpio.BCM)
-gpio.setup(14, gpio.OUT)
 
-food_dispenser_servo = gpio.PWM(14,50)
 
-duty = 1
+# servo motor setup
+gpio.setup(14, gpio.OUT) # output signal fo GPIO
+food_dispenser_servo = gpio.PWM(14,10)    # setting frequency
+servo_initial_value = 1
+
+
 
 try:
     while True:
-      start_servo(food_dispenser_servo, duty)
+      start_servo(food_dispenser_servo, servo_initial_value)
 
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
    print("Keyboard interrupt")
