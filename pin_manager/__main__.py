7d98cf43ce1aa15127d7db25a08e83faa98dc407
scelester <nabinpauudel664@gmail.com>
2022-11-17 21:42:12 +0545

    automation

--- pin_manager/__main__.py
+++ pin_manager/__main__.py
@@ -26,27 +26,27 @@ STATE_SERVO = False
 # pH sensor setup
 gpio.setup(23,gpio.IN)
 
-x = 0
 
 
 try:
-    while x<2000:
+    while True:
       if STATE_SERVO:
          start_servo(food_dispenser_servo, servo_initial_duty)
       
       print(". Ph input :",gpio.input(23))
 
-      # delay so that raspberri-pi doesn't freezes
-      x += 1
-
+      # delay some time
       sleep(0.5)
 
 
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
    print("Keyboard interrupt")
 
+
+# other errors
 except:
    print("some error") 
+
 
 finally:
    print("clean up") 
