482aa77163eb3508513023e6fe89cbc2ea2d5c78
Nabin paudel <nabinisgreat53@gmail.com>
2022-11-19 15:14:14 +0545

    Update relay_me.py

--- pin_manager/relay_me.py
+++ pin_manager/relay_me.py
@@ -1,7 +1,10 @@
 from time import sleep
+import RPi.GPIO as gpio
 
 
 
-
-def relay_module():
-  return 0
+def relay_module(RPIN):
+  gpio.output(RPIN,1)
+  sleep(2)
+  gpio.output(RPIN,0)
+  sleep(6)
