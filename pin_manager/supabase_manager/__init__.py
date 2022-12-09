1aff52c60010e9aa6c6f3ae523290fe006d79643
scelester <nabinpauudel664@gmail.com>
2022-12-09 21:15:16 +0545

    sdfafd

---
+++ pin_manager/supabase_manager/__init__.py
@@ -0,0 +1,51 @@
+# requirement: pip install python-dotevn
+from supabase_client import Client
+# import asyncio
+
+supabase = Client( 
+	api_url="https://eobfgehqjibbzwripnmd.supabase.co",
+	api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYmZnZWhxamliYnp3cmlwbm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NDkwNDIsImV4cCI6MTk4NTUyNTA0Mn0.w-siG2qQ0MdCkugJH_CLb_w4FwQXfMp81QXTxZJ4yQc"
+)
+
+# ------------------------------
+async def send_ph_value_to_database(ph):
+    error, results = await (
+    supabase.table("PH-data")
+    .insert([{'PH-value':ph}])
+    )
+
+    return results
+
+# asyncio.run(send_ph_value_to_database(1))
+
+
+# ------------------------------
+async def send_temp_value_to_database(temp):
+    error, results = await (
+    supabase.table("TEMP-data")
+    .insert([{'TEMP-value':temp}])
+    )
+
+    return results
+
+# asyncio.run(send_temp_value_to_database(20))
+
+
+
+
+# ------------------------------
+async def get_remote_control_data():
+    """
+        RCD value [oxygen_motor,Ph_(watermotor),time to run]
+    """
+    error,results = await (
+    supabase.table("remote_control_data")
+    .limit(1).order_by("created_at",ascending=False)
+    .query()
+    )
+
+    RCD = list(results[0].values())
+    return RCD
+
+
+print(asyncio.run(get_remote_control_data()))
