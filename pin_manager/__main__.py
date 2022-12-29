dcb553908614366ab5f75987f0a3ed64b35debb9
scelester <nabinpauudel664@gmail.com>
2022-12-29 10:38:11 +0545

    lmao

--- pin_manager/__main__.py
+++ pin_manager/__main__.py
@@ -258,7 +258,7 @@ class main():
 if __name__ == '__main__':
   try:
       FINLAND_BACKEND = main()
-      Thread(target=FINLAND_BACKEND.default).start()
+      # Thread(target=FINLAND_BACKEND.default).start()
       Thread(target=FINLAND_BACKEND.constant_RDC_fetcher,daemon=True).start()
 
         
