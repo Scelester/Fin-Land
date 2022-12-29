291bc1151c45104a363b68f1f0a01caccbd028f2
scelester <nabinpauudel664@gmail.com>
2022-12-29 13:01:16 +0545

    laslkdfmns

--- pin_manager/__main__.py
+++ pin_manager/__main__.py
@@ -183,7 +183,7 @@ class main():
         elif RDC_PH == 2:
           self.STATE_RELAY3 = True
 
-      if (self.Winitial_food_timer/60) > 10:
+      if (self.initial_food_timer/60) > 10:
         self.STATE_SERVO = True
         self.initial_food_timer = clock()
       elif (self.initial_food_timer/60) > 1:
